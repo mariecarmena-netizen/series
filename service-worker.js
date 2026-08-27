58ea19e0f34f4c8429075a95d279031b3ff3f691
mariecarmena-netizen <mariecarmena@gmail.com>
2026-08-27 22:40:31 +0200

    app

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,12 @@
-const CACHE_NAME="tus-series-v8",APP_SHELL=["./index.html","./manifest.webmanifest","./assets/app-icon-192.png","./assets/app-icon-512.png","./assets/app-icon-maskable-512.png","./assets/apple-touch-icon.png","./assets/favicon-32.png"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(e=>e.addAll(APP_SHELL)).then(()=>self.skipWaiting()))}),self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(e=>Promise.all(e.filter(e=>e!==CACHE_NAME).map(e=>caches.delete(e)))).then(()=>self.clients.claim()))}),self.addEventListener("fetch",e=>{"GET"===e.request.method&&("navigate"!==e.request.mode?e.respondWith(caches.match(e.request).then(t=>t||fetch(e.request))):e.respondWith(fetch(e.request).then(t=>{const s=t.clone();return caches.open(CACHE_NAME).then(t=>t.put(e.request,s)),t}).catch(()=>caches.match(e.request).then(e=>e||caches.match("./index.html")))))});
+const CACHE_NAME = "tus-series-v9",
+  APP_SHELL = ["./index.html", "./manifest.webmanifest", "./assets/app-icon-192.png", "./assets/app-icon-512.png", "./assets/app-icon-maskable-512.png", "./assets/apple-touch-icon.png", "./assets/favicon-32.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(e => e.addAll(APP_SHELL)).then(() => self.skipWaiting()))
+}), self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(e => Promise.all(e.filter(e => e !== CACHE_NAME).map(e => caches.delete(e)))).then(() => self.clients.claim()))
+}), self.addEventListener("fetch", e => {
+  "GET" === e.request.method && ("navigate" !== e.request.mode ? e.respondWith(caches.match(e.request).then(t => t || fetch(e.request))) : e.respondWith(fetch(e.request).then(t => {
+    const s = t.clone();
+    return caches.open(CACHE_NAME).then(t => t.put(e.request, s)), t
+  }).catch(() => caches.match(e.request).then(e => e || caches.match("./index.html")))))
+});
